13a52929cb37bd65fdd5619081edad26f58bb18d
BVD <Lcjsleeper@gmail.com>
2025-09-03 12:18:29 +0200

    exo 1 fait

---
+++ Exercice1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Exercice 1");
+
+// declarer une variable prix et une variable entiere quantité pour calculer et afficher le total
+double prix = 19.99;
+int quantite = 3;
+double total = prix * quantite;
+Console.WriteLine("Le total est: " + total);
