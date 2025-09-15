be9c70611696710e970f36bd730e8640c10d9048
BVD <Lcjsleeper@gmail.com>
2025-09-15 16:41:51 +0200

    Tableau

---
+++ tableaux/Program.cs
@@ -0,0 +1,8 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Tableau tableau = new Tableau();
+        tableau.AfficherPrenoms();
+    }
+}
---
+++ tableaux/tableau.cs
@@ -0,0 +1,11 @@
+public class Tableau
+{
+    string[] prenoms = { "Alice", "Bob", "Charlie", "Diana", "Ethan" };
+    public void AfficherPrenoms()
+    {
+        foreach (string prenom in prenoms)
+        {
+            Console.WriteLine(prenom);
+        }
+    }
+}
